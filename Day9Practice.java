3659b04189d0dd113f0316f05c9af747f980cd47
Tekrajrajawat <rajawatlucky513@gmail.com>
2026-08-25 12:38:11 +0530

    Add Day9Practice.java with string operations

---
+++ Day9Practice.java
@@ -0,0 +1,13 @@
+public class Day9Practice {
+    public static void main(String[] args) {
+        
+        String name = "Rahul";
+
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("First character: " + name.charAt(0));
+
+        String testString = "Rahul";
+        System.out.println(("Are they equal? " + name.equals(testString)));
+    }
+}
