50cbd0a82761dde85445f84f2c38d56fb82827fe
Tekrajrajawat <rajawatlucky513@gmail.com>
2026-09-01 11:54:44 +0530

    Create Day15Practice.java

---
+++ Day15Practice.java
@@ -0,0 +1,16 @@
+public class Day15Practice {
+    public static void main(String[] args) {
+        
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero!");
+        } finally {
+            System.out.println("This block always runs, no matter what.");
+        }
+        
+    }
+}
